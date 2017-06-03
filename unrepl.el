f19b654f5ca1d68646802ff53db4956edcd9a1b4
Arne Brasseur <arne@arnebrasseur.net>
2017-06-03 15:24:07 +0200

    Isolate state for individual repls, add basic elision support

--- unrepl.el
+++ unrepl.el
@@ -2,21 +2,19 @@
 
 ;; Copyright (C) 2017  Arne Brasseur
 
-;; Author: Arne Brasseur <arne@bmo>
+;; Author: Arne Brasseur <arne@arnebrasseur.net>
 ;; Keywords: tools, lisp, comm
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the Mozilla Public License Version 2.0
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the Mozilla Public License along with this
+;; program. If not, see <https://www.mozilla.org/media/MPL/2.0/index.txt>.
 
 ;;; Commentary:
 
@@ -24,25 +22,9 @@
 
 ;;; Code:
 
-(defun get-string-from-file (filePath)
-  "Return filePath's file content."
-  (with-temp-buffer
-    (insert-file-contents filePath)
-    (buffer-string)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq unrepl-home (file-name-directory load-file-name))
-
-(defun unrepl-test ()
-  load-file-name)
-
-(defun unrepl-read-blob ()
-  ;;TODO don't hard code this
-  (get-string-from-file (expand-file-name "blob.clj" unrepl-home)))
-
-(defvar-local unrepl-repl-input-start-mark nil)
-(defvar-local unrepl-hello-payload nil)
+(require 'edn)
+(require 'clojure-mode)
+(require 'dash)
 
 (edn-add-reader :unrepl/param (lambda (x) `(:unrepl/param . ,x)))
 (edn-add-reader :unrepl/ns (lambda (x) `(:unrepl/ns . ,x)))
@@ -53,6 +35,91 @@
 (edn-add-reader :unrepl.java/class (lambda (x) `(:unrepl.java/class . ,x)))
 (edn-add-reader :unrepl/... (lambda (x) `(:unrepl/... . ,x)))
 (edn-add-reader :error #'identity)
+
+(defvar unrepl-blob-path
+  (expand-file-name "blob.clj" (file-name-directory load-file-name))
+  "Path to the unrepl 'upgrade blob', created with `lein unrepl-make-blob'.")
+
+(defvar-local unrepl-repl-input-start-mark nil)
+(defvar-local unrepl-hello-payload nil)
+(defvar-local unrepl-buffer-type nil)
+
+(defvar unrepl-connections nil
+  "List of unrepl connection objects")
+
+(defvar-local unrepl-connection nil
+  "Connection used by the current buffer")
+
+;; borrowed from CIDER
+(defun unrepl-defun-at-point (&optional bounds)
+  "Return the text of the top level sexp at point.
+If BOUNDS is non-nil, return a list of its starting and ending position
+instead."
+  (save-excursion
+    (save-match-data
+      (end-of-defun)
+      (let ((end (point)))
+        (clojure-backward-logical-sexp 1)
+        (funcall (if bounds #'list #'buffer-substring-no-properties)
+                 (point) end)))))
+
+;; borrowed from CIDER
+(defun unrepl-last-sexp (&optional bounds)
+  "Return the sexp preceding the point.
+If BOUNDS is non-nil, return a list of its starting and ending position
+instead."
+  (apply (if bounds #'list #'buffer-substring-no-properties)
+         (save-excursion
+           (clojure-backward-logical-sexp 1)
+           (list (point)
+                 (progn (clojure-forward-logical-sexp 1)
+                        (skip-chars-forward "[:blank:]")
+                        (when (looking-at-p "\n") (forward-char 1))
+                        (point))))))
+
+
+(defun unrepl--elision? (x)
+  (and (listp x) (eq (first x) :unrepl/...)))
+
+(defun unrepl--insert-elision-button (edn)
+  (insert-button
+   "..."
+   'buffer (current-buffer)
+   'point (point)
+   'get-more (gethash :get (cdr edn))
+   'action (lambda (b)
+             (save-excursion
+               (with-current-buffer (button-get b 'buffer)
+                 (let-alist (unrepl--connection)
+                   (goto-char (button-get b 'point))
+                   (delete-char 3)
+                   (process-send-string .process (concat (edn-print-string (button-get b 'get-more)) "\n")))
+                   )))))
+
+
+(defun unrepl--insert-edn-list-inner (edn)
+  (-map-indexed (lambda (idx x)
+                  (when (> idx 0)
+                    (insert " "))
+                  (if (unrepl--elision? x)
+                      (unrepl--insert-elision-button x)
+                    (unrepl--insert-edn x)))
+                edn))
+
+(defun unrepl--insert-edn-list (edn)
+  (insert "(")
+  (unrepl--insert-edn-list-inner edn)
+  (insert ")"))
+
+(defun unrepl--insert-edn (edn)
+  (if (listp edn)
+      (unrepl--insert-edn-list edn)
+    (insert (edn-print-string edn))))
+
+(defun unrepl--read-blob ()
+  (with-temp-buffer
+    (insert-file-contents unrepl-blob-path)
+    (buffer-string)))
 
 (defun unrepl--insert-with-face (str face)
   (put-text-property 0 (length str) 'face face str)
@@ -84,9 +151,11 @@
 
 (defun unrepl--handle-eval (payload)
   (when (not (eq (current-column) 0))
-    (unrepl--insert-with-face "%" 'custom-set)
+    ;; taken out because when evaluating forms from the repl it would show up
+    ;; for no good reason
+    ;; (unrepl--insert-with-face "%" 'custom-set)
     (insert "\n"))
-  (insert (edn-print-string payload))
+  (unrepl--insert-edn payload)
   (insert "\n"))
 
 (defun unrepl--handle-bye (payload))
@@ -101,39 +170,41 @@
 
 (defun unrepl-edn-handler (form)
   ;;(message (concat "->" (prin1-to-string form)))
-  (with-current-buffer (get-buffer-create "*unrepl-repl*")
-    (let ((tag (elt form 0))
-          (payload (elt form 1)))
-      (case tag
-        (:unrepl/hello (unrepl--handle-hello payload))
-        (:prompt (unrepl--handle-prompt payload))
-        (:started-eval (unrepl--handle-started-eval payload))
-        (:eval (unrepl--handle-eval payload))
-        (:out (unrepl--handle-out payload))
-        (:err (unrepl--handle-err payload))
-        (:bye (unrepl--handle-bye payload))
-        (:log (unrepl--handle-log payload))
-        (:exception (unrepl--handle-exception payload))))))
+  (let-alist (unrepl--connection)
+    (with-current-buffer .repl-buffer
+      (let ((tag (elt form 0))
+            (payload (elt form 1)))
+        (case tag
+          (:unrepl/hello (unrepl--handle-hello payload))
+          (:prompt (unrepl--handle-prompt payload))
+          (:started-eval (unrepl--handle-started-eval payload))
+          (:eval (unrepl--handle-eval payload))
+          (:out (unrepl--handle-out payload))
+          (:err (unrepl--handle-err payload))
+          (:bye (unrepl--handle-bye payload))
+          (:log (unrepl--handle-log payload))
+          (:exception (unrepl--handle-exception payload)))))))
 
 (defun unrepl-handle-output (proc string)
-  (with-current-buffer (get-buffer-create "*unrepl-output*")
-    (let ((orig-point (point)))
-      (insert string)
-      (goto-char orig-point)
-      ;;(message (prin1-to-string (list :ok orig-point (eq orig-point 0) string)))
+  (let-alist (unrepl--connection)
+    (with-current-buffer .output-buffer
+      (goto-char (point-max))
+      (let ((orig-point (point)))
+        (insert string)
+        (goto-char orig-point)
+        ;;(message (prin1-to-string (list :ok orig-point (eq orig-point 0) string)))
 
-      (when (eq orig-point 1)
-        (right-char 7)
-        (process-send-string unrepl-process (unrepl-read-blob))
-        ;; (search-forward "[:unrepl/hello")
-        ;; (left-char (length "[:unrepl/hello"))
-        )
+        (when (eq orig-point 1)
+          (right-char 7)
+          (process-send-string .process (unrepl--read-blob))
+          ;; (search-forward "[:unrepl/hello")
+          ;; (left-char (length "[:unrepl/hello"))
+          )
 
-      (let ((form (edn-read)))
-        (while form
-          (unrepl-edn-handler form)
-          (setq form (edn-read)))))))
-
+        (let ((form (edn-read)))
+          (while form
+            (unrepl-edn-handler form)
+            (setq form (edn-read))))))))
 
 (defun unrepl-process-sentinel (proc event)
   ;;(message (concat "EVT->" (prin1-to-string event)))
@@ -148,44 +219,35 @@
     (setq unrepl-repl-input-start-mark (point))
     (add-text-properties prompt-start (point)
                          '(font-lock-face font-lock-keyword-face
-                           read-only t
-                           intangible t
-                           ;; field cider-repl-prompt
-                           rear-nonsticky (field read-only font-lock-face intangible)))))
+                                          read-only t
+                                          intangible t
+                                          ;; field cider-repl-prompt
+                                          rear-nonsticky (field read-only font-lock-face intangible)))))
 
+
+(defun unrepl--connection ()
+  ;; TODO make this smarter, so buffers find the most suitable connection based
+  ;; on path/project
+  (or unrepl-connection (first unrepl-connections)))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Commands and Modes
 
 (defun unrepl-repl-return ()
   (interactive)
-  (insert "\n")
-  (condition-case nil
-      (let ((cmd (buffer-substring-no-properties unrepl-repl-input-start-mark (point-max))))
-        (edn-read cmd)
-        (process-send-string unrepl-process cmd))
-    (error (insert "#_=> "))))
-
-(defun unrepl-start (command)
-  (when (get-buffer "*unrepl-output*")
-    (kill-buffer "*unrepl-output*"))
-
-  (with-current-buffer (get-buffer-create "*unrepl-repl*")
-    (unrepl-repl-mode))
-
-  (setq unrepl-process (make-process
-                        :name "unrepl"
-                        :buffer "*unrepl-process*"
-                        :command command
-                        :filter #'unrepl-handle-output
-                        :sentinel #'unrepl-process-sentinel
-                        :connection-type 'pipe)))
+  (when (eq (point) (line-end-position))
+    (let-alist (unrepl--connection)
+      (insert "\n")
+      (condition-case nil
+          (let ((cmd (buffer-substring-no-properties unrepl-repl-input-start-mark (point-max))))
+            (edn-read cmd)
+            (process-send-string .process cmd))
+        (error (insert "#_=> "))))))
 
 (defun unrepl-connect (host-port)
   (interactive "MSocket repl host/port: ")
-
-  (when (get-buffer "*unrepl-output*")
-    (kill-buffer "*unrepl-output*"))
-
-  (with-current-buffer (get-buffer-create "*unrepl-repl*")
-    (unrepl-repl-mode))
 
   (let* ((host-port (s-split ":" host-port))
          (host-given? (= (length host-port) 2))
@@ -195,30 +257,60 @@
          (port (string-to-number
                 (if host-given?
                     (second host-port)
-                  (first host-port)))))
-    (setq unrepl-process (make-network-process
-                          :name "unrepl"
-                          :buffer "*unrepl-process*"
-                          :host host
-                          :service port
-                          :filter #'unrepl-handle-output
-                          :sentinel #'unrepl-process-sentinel))
-    (switch-to-buffer "*unrepl-repl*")))
+                  (first host-port))))
+         (output-buffer-name (if unrepl-connections
+                                 (concat "*unrepl-output<" (number-to-string (1+ (length unrepl-connections))) ">*")
+                               "*unrepl-output*"))
+         (repl-buffer-name (if unrepl-connections
+                               (concat "*unrepl-repl<" (number-to-string (1+ (length unrepl-connections))) ">*")
+                             "*unrepl-repl*")))
+
+    (when (get-buffer output-buffer-name)
+      (kill-buffer output-buffer-name))
+
+    (let* ((process (make-network-process
+                     :name "unrepl"
+                     :buffer output-buffer-name
+                     :host host
+                     :service port
+                     :filter #'unrepl-handle-output
+                     :sentinel #'unrepl-process-sentinel))
+           (repl-buffer (get-buffer-create repl-buffer-name))
+           (output-buffer (get-buffer output-buffer-name))
+           (connection `((process . ,process)
+                         (repl-buffer . ,repl-buffer)
+                         (output-buffer . ,output-buffer))))
+
+      (with-current-buffer output-buffer
+        (setq unrepl-connection connection)
+        (setq unrepl-buffer-type 'process))
+
+      (with-current-buffer repl-buffer
+        (unrepl-repl-mode)
+        (setq unrepl-connection connection)
+        (setq unrepl-buffer-type 'repl))
+
+      (push connection unrepl-connections)
+      (switch-to-buffer repl-buffer))))
 
 (defun unrepl-eval-last-sexp ()
   (interactive)
-  (process-send-string unrepl-process (concat (cider-last-sexp) "\n")))
+  (let-alist (unrepl--connection)
+    (process-send-string .process (concat (unrepl-last-sexp) "\n"))))
 
-(defun unrepl-close ()
+(defun unrepl-eval-defun ()
   (interactive)
-  (when (get-buffer "*unrepl-output*")
-    (kill-buffer "*unrepl-output*"))
-  (when (get-buffer "*unrepl-repl*")
-    (kill-buffer "*unrepl-repl*"))
-  (when (get-buffer "*unrepl-process*")
-    (kill-buffer "*unrepl-process*")))
+  (let-alist (unrepl--connection)
+    (process-send-string .process (concat (unrepl-defun-at-point) "\n"))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun unrepl-close-all ()
+  (interactive)
+  (-map (lambda (conn)
+          (let-alist conn
+            (kill-buffer .repl-buffer)
+            (kill-buffer .output-buffer)))
+        unrepl-connections)
+  (setq unrepl-connections nil))
 
 (defvar unrepl-repl-mode-map
   (let ((map (make-sparse-keymap)))
@@ -237,16 +329,29 @@
   "unrepl"
   unrepl-mode-map)
 
-;; (progn
-;;   (when (get-buffer "*unrepl-process*")
-;;     (kill-buffer "*unrepl-process*"))
-;;   (when (get-buffer "*unrepl-repl*")
-;;     (kill-buffer "*unrepl-repl*"))
-;;   (unrepl-start '("telnet" "localhost" "3848")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Hooks
 
-;; to be able to start/stop repls quickly for testing, I start unrepl on a socket repl server, then connect to it with telnet
-;;
-;; JAVA_OPTS="-Dclojure.server.unrepl={:address \"127.0.0.1\"  :port 3848 :accept unrepl.repl/start}" lein run -m clojure.main/main
+(defun unrepl--kill-buffer-hook-handler ()
+  "Clean up when closing either the repl or the output/process
+buffer. Close the other buffer, and remove the connection from
+the list."
+  (when (or (eq unrepl-buffer-type 'repl) (eq unrepl-buffer-type 'process))
+    (let-alist (unrepl--connection)
+      (delete-process .process)
+      (setq unrepl-connections (-remove (lambda (conn)
+                                          (eq conn (unrepl--connection)))
+                                        unrepl-connections))
+      (if (eq unrepl-buffer-type 'repl)
+          (with-current-buffer .output-buffer
+            (setq unrepl-buffer-type nil)
+            (kill-buffer .output-buffer))
+        (with-current-buffer .repl-buffer
+          (setq unrepl-buffer-type nil)
+          (kill-buffer .repl-buffer))))))
+
+(add-hook 'kill-buffer-hook 'unrepl--kill-buffer-hook-handler)
 
 (provide 'unrepl)
 ;;; unrepl.el ends here
