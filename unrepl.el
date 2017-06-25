4b266dee5f68a7f6ead350462ef33ad37c10aa43
Arne Brasseur <arne@arnebrasseur.net>
2017-06-25 19:57:18 +0200

    Track history, do eval+callback
    Instead of handling all eval results the same, keep track of sent commands, link
    up offset with id's and id's with eval results, so a specific result can be
    linked back to a specific handler for it.

--- unrepl.el
+++ unrepl.el
@@ -35,7 +35,8 @@
   (expand-file-name "blob.clj" (file-name-directory load-file-name))
   "Path to the unrepl 'upgrade blob', created with `lein unrepl-make-blob'.")
 
-(defvar-local unrepl-repl-input-start-mark nil)
+(defvar-local unrepl-input-start-mark nil)
+(defvar-local unrepl-prompt-end-mark nil)
 (defvar-local unrepl-hello-payload nil)
 (defvar-local unrepl-buffer-type nil)
 
@@ -43,7 +44,7 @@
   "List of unrepl connection objects")
 
 (defvar-local unrepl-connection nil
-  "Connection used by the current buffer")
+  "Connection used by the current buffer. An alist of process, repl-buffer, output-buffer, position, eval-handlers.")
 
 ;; borrowed from CIDER
 (defun unrepl-defun-at-point (&optional bounds)
@@ -81,6 +82,22 @@ instead."
   (put-text-property 0 (length str) 'face face str)
   (insert str))
 
+(defun unrepl--history-idx (key value)
+  (let* ((conn (unrepl--connection))
+         (history (cdr (assq 'history conn))))
+    (seq-position history value
+                  (lambda (x y)
+                    (eq (cdr (assq key x)) y)))))
+
+(defun unrepl--history-by (key value)
+  (seq-elt (cdr (assq 'history (unrepl--connection)))
+           (unrepl--history-idx key value)))
+
+(cl-defmethod (setf unrepl--history-by) (cmd key value)
+  (setf (seq-elt (cdr (assq 'history (unrepl--connection)))
+                 (unrepl--history-idx key value))
+        cmd))
+
 (defun unrepl--handle-hello (payload)
   (setq unrepl-hello-payload payload))
 
@@ -92,9 +109,6 @@ instead."
    (symbol-name
     (third (gethash 'clojure.core/*ns* payload)))))
 
-;; TODO store the payload so we can :interrupt or :background
-(defun unrepl--handle-started-eval (payload))
-
 (defun unrepl--handle-out (payload)
   (if (eq (current-column) 0)
       (unrepl--insert-with-face "#_out| " 'font-lock-constant-face))
@@ -105,14 +119,29 @@ instead."
       (unrepl--insert-with-face "#_err| " 'font-lock-warning-face))
   (insert (ansi-color-apply payload)))
 
-(defun unrepl--handle-eval (payload)
-  (when (not (eq (current-column) 0))
-    ;; taken out because when evaluating forms from the repl it would show up
-    ;; for no good reason
-    ;; (unrepl--insert-with-face "%" 'custom-set)
-    (insert "\n"))
-  (unrepl--write payload)
-  (insert "\n"))
+;; [:read {:from [2 1], :to [3 1], :offset 8, :len 8} 2]
+(defun unrepl--handle-read (form)
+  (seq-let [_ pos-info id] form
+    (let* ((offset (gethash :offset pos-info)))
+      (push (cons 'id id) (unrepl--history-by 'position offset)))))
+
+;; [:started-eval {:actions {:interrupt (unrepl.replG__1801/interrupt! :session2160 2), :background (unrepl.replG__1801/background! :session2160 2)}} 2]
+(defun unrepl--handle-started-eval (payload))
+
+;; [:eval #_result 14 #_id 2]
+(defun unrepl--handle-eval (form)
+  (seq-let [_ result id] form
+    (push (cons 'result result) (unrepl--history-by 'id id))
+    (funcall (cdr (assq 'handler (unrepl--history-by 'id id))) result))
+
+  ;; (when (not (eq (current-column) 0))
+  ;;   ;; taken out because when evaluating forms from the repl it would show up
+  ;;   ;; for no good reason
+  ;;   ;; (unrepl--insert-with-face "%" 'custom-set)
+  ;;   (insert "\n"))
+  ;;(unrepl--write payload)
+  ;;(insert "\n")
+  )
 
 (defun unrepl--handle-bye (payload))
 
@@ -133,8 +162,9 @@ instead."
         (case tag
           (:unrepl/hello (unrepl--handle-hello payload))
           (:prompt (unrepl--handle-prompt payload))
-          (:started-eval (unrepl--handle-started-eval payload))
-          (:eval (unrepl--handle-eval payload))
+          (:read (unrepl--handle-read form))
+          (:started-eval (unrepl--handle-started-eval form))
+          (:eval (unrepl--handle-eval form))
           (:out (unrepl--handle-out payload))
           (:err (unrepl--handle-err payload))
           (:bye (unrepl--handle-bye payload))
@@ -158,7 +188,7 @@ instead."
           )
 
         (let ((form (unrepl--edn-read)))
-          (prin1 (list "=>" form))
+          ;;(prin1 (list "=>" form))
           (while form
             (unrepl-edn-handler form)
             (setq form (unrepl--edn-read))))))))
@@ -173,13 +203,14 @@ instead."
       (insert "\n"))
   (let ((prompt-start (point)))
     (insert (concat namespace "=> "))
-    (setq unrepl-repl-input-start-mark (point))
+    (setq unrepl-input-start-mark (point))
+    (setq unrepl-prompt-end-mark (point))
     (add-text-properties prompt-start (point)
                          '(font-lock-face font-lock-keyword-face
-                                          read-only t
-                                          intangible t
-                                          ;; field cider-repl-prompt
-                                          rear-nonsticky (field read-only font-lock-face intangible)))))
+                           read-only t
+                           intangible t
+                           front-sticky t
+                           rear-nonsticky t))))
 
 
 (defun unrepl--connection ()
@@ -196,13 +227,31 @@ instead."
   (interactive)
   (when (eq (point) (line-end-position))
     (let-alist (unrepl--connection)
+      (message "before")
+      (let ((inhibit-read-only t))
+        (add-text-properties unrepl-prompt-end-mark (point-max)
+                             '(read-only t
+                               front-sticky t
+                               rear-nonsticky t)))
+      (message "after")
       (insert "\n")
-      (condition-case nil
-          (let ((cmd (buffer-substring-no-properties unrepl-repl-input-start-mark (point-max))))
-            ;;(unrepl--edn-read cmd)
-            (process-send-string .process cmd)
-            )
-        (error (insert "#_=> "))))))
+      (message "newline")
+      (let ((cmd (buffer-substring-no-properties unrepl-input-start-mark (point-max))))
+        (setq unrepl-input-start-mark (point))
+        (unrepl-eval cmd #'unrepl--write)))))
+
+(defun unrepl-eval (form handler)
+  (message "Eval %S" form)
+  (let* ((connection (unrepl--connection))
+         (position   (assq 'position connection))
+         (history    (assq 'history connection))
+         (process    (assq 'process connection)))
+    (push `((position . ,(cdr position))
+            (form . ,form)
+            (handler . ,handler))
+          (cdr history))
+    (process-send-string (cdr process) form)
+    (setcdr position (+ (length form) (cdr position)))))
 
 (defun unrepl-connect (host-port)
   (interactive "MSocket repl host/port: ")
@@ -237,7 +286,9 @@ instead."
            (output-buffer (get-buffer output-buffer-name))
            (connection `((process . ,process)
                          (repl-buffer . ,repl-buffer)
-                         (output-buffer . ,output-buffer))))
+                         (output-buffer . ,output-buffer)
+                         (position . 0)
+                         (history . nil))))
 
       (with-current-buffer output-buffer
         (setq unrepl-connection connection)
