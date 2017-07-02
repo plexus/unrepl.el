ecfbb05ec7f3d656deb15834f8ede4c7b634268b
Александар Симић <asimic@gmail.com>
2017-07-02 08:42:00 +0100

    Remove the mention of GPL
    Seeing how this package is under MPL, tidying up the header

--- unrepl-reader.el
+++ unrepl-reader.el
@@ -9,8 +9,7 @@
 
 ;; This program is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-;; details.
+;; FOR A PARTICULAR PURPOSE. See the Mozilla Public License for more details.
 
 ;; You should have received a copy of the Mozilla Public License along with this
 ;; program. If not, see <https://www.mozilla.org/media/MPL/2.0/index.txt>.
